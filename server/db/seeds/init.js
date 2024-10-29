c9ad1634211d8b78fa7094e31ac25b2b34921400
Z-noob <Zhenniwu5@gmail.com>
2024-10-29 17:39:47 -0400

    seed file

--- server/db/seeds/init.js
+++ server/db/seeds/init.js
@@ -5,9 +5,20 @@ const User = require('../../models/User');
  */
 exports.seed = async (knex) => {
   // Before you have models you can always just do `await knex('table_name').del`
-  await knex('users').del();
-
-  await knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
+    // Clear existing data and reset sequences
+    await knex('transaction_history').del();
+    await knex('random_events').del();
+    await knex('budgets').del();
+    await knex('simulations').del();
+    await knex('users').del();
+    await knex('user_profile').del();
+  
+    await knex.raw('ALTER SEQUENCE transaction_history_id_seq RESTART WITH 1');
+    await knex.raw('ALTER SEQUENCE random_events_id_seq RESTART WITH 1');
+    await knex.raw('ALTER SEQUENCE budgets_id_seq RESTART WITH 1');
+    await knex.raw('ALTER SEQUENCE simulations_id_seq RESTART WITH 1');
+    await knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
+    // await knex.raw('ALTER SEQUENCE user_profile_id_seq RESTART WITH 1');
 
   // User.create(username, password)
   await User.create('cool_cat', '1234');
